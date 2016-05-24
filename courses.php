635cc32a99a798bac9aa6c694e3dc9456b034ec5
harsh91 <harsh_fresno@mail.fresnostate.edu>
2016-05-24 14:34:01 -0700

    Completed courses page.

---
+++ courses.php
@@ -0,0 +1,45 @@
+<html>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Courses</title>
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+		<link rel="stylesheet" type="text/css" href="css/homepage.css" />
+		<script src="js/jquery.js"></script>
+		<script src="js/bootstrap.js"></script>
+	</head>
+	<body>
+		<?php include("header.html"); ?>
+		<section class="main-container container courses-section">
+			<div class="col-lg-6 col-sm-6 col-xs-12 achievements">
+				<h4>University of Louisiana at Lafayette</h4>
+				<ul>
+					<li>VLSI Design (EECE 585)</li>
+					<li>Image Processing (EECE 508)</li>
+					<li>VLSI Architecture (EECE 586)</li>
+					<li>Computer Architecture (EECE 530)</li>
+					<li>Operating System Theory (EECE 555)</li>
+					<li>Design & Analysis of Algorithms (EECE 500)</li>
+					<li>Computer Communication and Networks (EECE 513)</li>
+				</ul>
+				<h4>West Valley Mission College</h4>
+				<ul>
+					<li>Linux Essentials I (CIS045)</li>
+					<li>Mobile Apps Programming - iPhone (CIS060)</li>
+				</ul>
+				<h4>California State University, Fresno</h4>
+				<ul>
+					<li>Digital Logic Design (ECE085)</li>
+					<li>Switching Theory and Logical Design (ECE106)</li>
+					<li>Computer Organization (ECE115)</li>
+					<li>Advanced Computer Architecture (ECE174)</li>
+					<li>Mobile Applications - Lab (ECE122L)</li>
+					<li>Engineering Computations Using C (ECE70)</li>
+					<li>Advanced Computer Networking (ECE246)</li>
+					<li>High Performance Computer Architecture (ECE274)</li>
+				</ul>
+			</div>
+		</section>
+		<?php include("footer.html"); ?>
+		<!-- Body content ends here -->
+	</body>
+</html>
