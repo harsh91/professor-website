e2c71883b353a2d4416b8d5aaa07571015182af9
harsh91 <harsh_fresno@mail.fresnostate.edu>
2016-05-24 14:36:58 -0700

    Adding video lecture page

---
+++ videos.php
@@ -0,0 +1,24 @@
+<html>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Videos</title>
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+		<link rel="stylesheet" type="text/css" href="css/homepage.css" />
+		<script src="js/jquery.js"></script>
+		<script src="js/bootstrap.js"></script>
+	</head>
+	<body>
+		<?php include("header.html"); ?>
+		<section class="main-container container section-2">
+			<object data="http://www.youtube.com/embed/l5cVDl5BFTc"
+			width="560" height="315"></object>
+			<object data="http://www.youtube.com/embed/l5cVDl5BFTc"
+			width="560" height="315"></object>
+			<object data="http://www.youtube.com/embed/l5cVDl5BFTc"
+			width="560" height="315"></object>
+			<object data="http://www.youtube.com/embed/l5cVDl5BFTc"
+			width="560" height="315"></object>
+		</section>
+		<?php include("footer.html"); ?>
+	</body>
+</html>
